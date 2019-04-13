9ce92d7e95ce9ff01c6b03bb932c38c242e06ae2
Tim Chosen <timchosen@gmail.com>
2019-04-13 12:38:21 +0100

    changed some issues with css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
+// import "jquery";
+// import * as $ from 'jquery';
+// import "bootstrap/dist/js/bootstrap.min.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
